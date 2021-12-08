6dc0d5e0e2523ca6ce77cd97747fc7ffd28eabaa
Skyler Crane <skylercrane7@gmail.com>
2021-12-08 15:36:34 -0700

    Adjusted routes to have navbar component, and konami and giphy pages.

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -1,26 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { css } from '@emotion/core'
-import { Home } from './home'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Global } from '@emotion/react'
+import { NavBar } from './components/navigation/NavBar'
+import { Konami } from './pages/Konami'
+import { Giphy } from './pages/Giphy'
 
 function AppRouter () {
   return (
     <Router>
-      <div css={layoutStyle}>
-        <nav css={navStyle}>
-          <ul >
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/another'>Another route</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className='main-content' css={contentStyle}>
-          <Route component={Home} exact path='/' />
-          <Route component={() => (<div>Content for /another route</div>)} exact path='/another' />
-        </div>
+      <Global styles={globalStyle} />
+      <NavBar />
+      <div>
+        <Route component={Konami} exact path='/' />
+        <Route component={Giphy} exact path='/giphy' />
       </div>
     </Router>
   )
@@ -28,26 +20,106 @@ function AppRouter () {
 
 export default AppRouter
 
-const layoutStyle = css`
-    display: grid;
-    grid-row-gap: 24px;
-    padding: 8px;
-`
+const globalStyle = `
+font-family: sans-serif;
+/* http://meyerweb.com/eric/tools/css/reset/
+   v2.0 | 20110126
+   License: none (public domain)
+*/
 
-const navStyle = css`
-  grid-row: 1;
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+margin: 0;
+padding: 0;
+border: 0;
+font-size: 100%;
+font: inherit;
+vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+display: block;
+}
+body {
+line-height: 1;
+font-family: "CalibreRegular", sans-serif;
+}
+ol, ul {
+list-style: none;
+}
+blockquote, q {
+quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+content: '';
+content: none;
+}
+table {
+border-collapse: collapse;
+border-spacing: 0;
+}
+h1, h2, h3, h4, h5, h6 {
+  font-family: serif;
+  font-weight: bold;
+}
+p, li {
+    font-family: 'Roboto', sans-serif;
+    line-height: 1.35;
+}
+h1 {
+    font-size: 4rem;
+}
+h2 {
+    font-size: 3.5rem;
+}
+h3 {
+    font-size: 3rem;
+}
+h4 {
+    font-size: 2.5rem;
+}
+h5 {
+    font-size: 2rem;
+}
+h6 {
+    font-size: 1.5rem;
+}
 
-  & > ul {
-      display: flex;
-      flex-direction: row;
-      list-style-type: none;
+.btn {
+  text-transform: uppercase;
+  text-align: center;
+  width: fit-content;
+  padding: 15px 22px;
+  border-radius: 50px;
+  text-decoration: none;
+  margin-top: 24px;
+  border: none;
+
+  &.btn-black {
+    color: #ffffff;
+    background: #000000;
   }
-  
-  & > ul > li:not(:first-of-type) {
-    margin-left: 16px;
-  }
-`
 
-const contentStyle = css`
-  grid-row: 2;
+  &.btn-blue {
+    color: #ffffff;
+    background: #2355be;
+  }
+
+  &.btn-sm {
+      font-size: 12px;
+  }
+}
 `
